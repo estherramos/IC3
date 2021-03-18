0e46d35f8e9009c6af6684c88e366d64ce8f71b1
estherramos <esther.ramos4d@gmail.com>
2021-03-19 00:15:21 +0100

    Cambio en las señales y añadidas en el documento

--- Ejercicio1/ejercicio1_e.vhd
+++ Ejercicio1/ejercicio1_e.vhd
@@ -11,30 +11,15 @@ architecture ejercicio1_e of ejercicio1_e is
     component ejercicio1_a is
         port (
             x,y,z: in std_logic;
-            F1,F2: out std_logic
-        );
+            F1,F2: out std_logic);
     end component ejercicio1_a;
     begin
         uut: component ejercicio1_a port map (x,y,z,F1,F2);
         test : process
         begin
-            x <= '0'; y <= '0'; z <= '0';
-            wait for 10 ns;
-            x <= '0'; y <= '0'; z <= '1';
-            wait for 10 ns;
-            x <= '0'; y <= '1'; z <= '0';
-            wait for 10 ns;
-            x <= '0'; y <= '1'; z <= '1';
-            wait for 10 ns;
-            x <= '1'; y <= '0'; z <= '0';
-            wait for 10 ns;
-            x <= '1'; y <= '0'; z <= '1';
-            wait for 10 ns;
-            x <= '1'; y <= '1'; z <= '0';
-            wait for 10 ns;
-            x <= '1'; y <= '1'; z <= '1';
-            wait for 10 ns;
-            report "Fin de la simulación";
+            x <= '0', '1' after 40 ns ; 
+            y <= '0', '1' after 20 ns, '0' after 40 ns, '1' after 60 ns; 
+            z <= '0', '1' after 10 ns, '0' after 20 ns, '1' after 30 ns, '0' after 40 ns, '1' after 50 ns, '0' after 60 ns, '1' after 70 ns ; 
             wait;
         end process test;
 end architecture ejercicio1_e;
